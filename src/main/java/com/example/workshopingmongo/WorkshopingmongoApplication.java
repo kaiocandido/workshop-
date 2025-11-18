7f4239e10380d6da6d39f11c0f2cbe11ce55f709
adm <kaio.candf@gmail.com>
2025-11-18 14:59:41 -0300

    primeiro commit, iniciando projeto

---
+++ src/main/java/com/example/workshopingmongo/WorkshopingmongoApplication.java
@@ -0,0 +1,13 @@
+package com.example.workshopingmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopingmongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopingmongoApplication.class, args);
+	}
+
+}
